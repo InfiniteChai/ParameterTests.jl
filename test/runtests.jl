cde3afa50effa25f4a574369a53710434091e3ab
Iain Skett <iain.skett@gmail.com>
2021-05-02 22:39:10 +0100

    Extend test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,31 +8,42 @@ using Dates: Date
             @given a ∈ integers(Int8)
             @given b ∈ integers(; min=0)
             @given c ∈ integers(Int32; min=-10, max=10)
+            @given d ∈ integers(Int64; max=3)
             @test isa(a, Int8)
             @test isa(b, Int) && b >= 0
             @test isa(c, Int32) && -10 <= c <= 10
+            @test isa(d, Int64) && d <= 3
         end
 
         @paramtest "Floats" begin
             @given a ∈ floats(Float16)
-            @test isa(a, Float16) && !isnan(a) && !isinf(a)
-
             @given b ∈ floats(Float16; min=0, max=1)
+            @given c ∈ floats(; allow_nan=true, allow_inf=true)
+            @given d ∈ floats(Float32; allow_nan=false, allow_inf=true)
+            @test isa(a, Float16) && !isnan(a) && !isinf(a)
             @test isa(b, Float16) && 0 <= b <= 1
+            @test isa(c, Float64)
+            @test isa(d, Float32)
         end
 
         @paramtest "Dates" begin
             @given a ∈ dates()
             @given b ∈ dates(;min=Date(2020,1,1))
             @given c ∈ dates(;min=Date(2020,1,1), max=Date(2021,1,1))
+            @given d ∈ dates(;max=Date(2020,1,1))
             @test isa(a, Date)
             @test isa(b, Date) && b >= Date(2020,1,1)
             @test isa(c, Date) && Date(2020,1,1) <= c <= Date(2021,1,1)
+            @test isa(d, Date) && d <= Date(2020,1,1)
         end
 
         @paramtest "Strings" begin
-            @given a ∈ strings(:alpha; minsize=0, maxsize=10)
+            @given a ∈ strings(; minsize=0, maxsize=10)
+            @given b ∈ strings(:alphanum; minsize=3000)
+            @given c ∈ strings(['A','B']; maxsize=3)
             @test length(a) <= 10
+            @test 3000 <= length(b) <= 5000
+            @test length(c) <= 3 && Set(c) ⊆ ('A','B')
         end
     end
 
@@ -52,9 +63,13 @@ using Dates: Date
         @paramtest "Vectors" begin
             @given a ∈ vectors(integers(); maxsize=5)
             @given b ∈ vectors(dates(;min=Date(2020,1,1)); minsize=5, maxsize=10)
+            @given c ∈ vectors(floats(); minsize=10)
+            @given d ∈ vectors(floats(Float32;min=0, max=10))
             @test isa(a, Vector{Int}) && length(a) <= 5
             @test isa(b, Vector{Date}) && 5 <= length(b) <= 10
             @test all(map(x -> x >= Date(2020,1,1), b))
+            @test isa(c, Vector{Float64}) && length(c) >= 10
+            @test isa(d, Vector{Float32})
         end
     end
 
